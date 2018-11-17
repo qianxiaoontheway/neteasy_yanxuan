b210156b830557f0a8df2e2e6faf072d383c7b5b
qianxiaoontheway <904884556@qq.com>
2018-11-17 16:08:58 +0800

    wepy

---
+++ wepy-shop/src/api/api.js
@@ -0,0 +1,11 @@
+// application interface
+// Sever Web Application
+import{
+    wxRequest
+} from 
+"@/utils/wxRequest";
+const apiMall = 'https://sujiefs.com'
+const getAdList = (params)=>wxRequest(params,apiMall +'/api/adverts/list');
+export default {
+    getAdList
+}
